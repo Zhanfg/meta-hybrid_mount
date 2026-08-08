eab7330f30ade629471dfebd8fbe12199528384a
Axymorrsen <Axymosen@outlook.com>
2026-08-09 02:48:32 +0800

    webui: compose VFS card into config page

---
+++ webui/src/routes/ConfigPage.tsx
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+import ConfigTab from "./ConfigTab";
+import VfsConfigCard from "./VfsConfigCard";
+
+export default function ConfigPage() {
+  return (
+    <>
+      <ConfigTab />
+      <VfsConfigCard />
+    </>
+  );
+}
