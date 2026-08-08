cc1d8b6c460d9a970af2b1683d2a69f46b815326
Axymorrsen <Axymosen@outlook.com>
2026-08-09 02:48:44 +0800

    webui: compose VFS card into status page

---
+++ webui/src/routes/StatusPage.tsx
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+import StatusTab from "./StatusTab";
+import VfsStatusCard from "./VfsStatusCard";
+
+export default function StatusPage() {
+  return (
+    <>
+      <StatusTab />
+      <VfsStatusCard />
+    </>
+  );
+}
